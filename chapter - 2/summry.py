ff35416d9cde4f7a1764bb3bbf3e4d9554e30acd
uvpatel <uvpatel7271@gmail.com>
2025-03-13 12:09:47 +0530

    Delete chapter - 2 directory

--- chapter - 2/summry.py
+++
@@ -1,9 +0,0 @@
-'''
-arithmatic - math operations
-comparison - compare values
-logical - combine results
-assignment - assign values
-identity - compare memory location
-membership - check values in sequence
-bitwise - perform bitwise operations
-'''
